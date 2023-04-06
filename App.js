6ad71fcab7d33484d6e371ba07fd1b7216be978b
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-04-07 01:21:00 +0300

    add register form

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        style={styles.image}
+        source={require("./assets/images/photo-bg.jpg")}
+      >
+        <View style={styles.regWrapper}>
+          <View style={styles.form}>
+            <Text style={styles.formTitle}> Регистрация </Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Логин"
+              placeholderTextColor="#BDBDBD"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Адрес электронной почты"
+              placeholderTextColor="#BDBDBD"
+            />
+            <TextInput
+              style={[styles.input, styles.inputLastChild]}
+              placeholder="Пароль"
+              placeholderTextColor="#BDBDBD"
+            />
+          </View>
+          <TouchableOpacity activeOpacity={0.8} style={styles.buttun}>
+            <Text style={styles.buttonTitle}>Зарегистрироваться</Text>
+          </TouchableOpacity>
+          <Text style={styles.signInLink}> Уже есть аккаунт? Войти </Text>
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +48,71 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  regWrapper: {
+    // borderWidth: 1,
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingTop: 92,
+    paddingBottom: 78,
+    paddingHorizontal: 16,
+  },
+  form: {
+    // paddingHorizontal: 16,
+  },
+  formTitle: {
+    marginBottom: 32,
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: "center",
+    letterSpacing: 0.01,
+    color: "#212121",
+  },
+  input: {
+    marginBottom: 16,
+    height: 50,
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    padding: 16,
+  },
+  inputLastChild: {
+    marginBottom: 53,
+  },
+  buttun: {
+    paddingHorizontal: 32,
+    paddingVertical: 16,
+    height: 51,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    marginBottom: 16,
+  },
+  buttonTitle: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: 400,
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center",
+    color: "#FFFFFF",
+  },
+  signInLink: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: 400,
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center",
+    color: "#1B4371",
   },
 });
